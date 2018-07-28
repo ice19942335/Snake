6dca966a9d4304991ad082d3a490627a026afb53
Aleksejs Birula <ice19942335@gmail.com>
2018-07-28 15:12:14 +0100

    Lesson 3

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        //Поля класса
+        public int x;
+        public int y;
+        public char sym;
+
+        //Конструктор со значениями по умолчанию
+        public Point(int x = 0, int y = 0, char sym = '?')
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
+        //Метод установки курсора в консоли и вывода символа
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+    }
+}
---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 2, '#');
+            p1.Draw();
+
+            Point p2 = new Point(5, 4, '*');
+            p2.Draw();
+
+            Console.ReadKey();
+        }
+    }
+}
