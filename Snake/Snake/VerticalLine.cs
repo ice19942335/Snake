641f2f12998545125fe57cb5b1e8bae5379cf6f3
Aleksejs Birula <ice19942335@gmail.com>
2018-07-28 17:43:03 +0100

    Changes in class

--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -10,10 +10,10 @@ namespace Snake
     {
         List<Point> pList;
 
-        public VerticalLine(int yLeft, int yRight, int x, char sym)
+        public VerticalLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
-            for (int y = yLeft; y <= yRight; y++)
+            for (int y = yUp; y <= yDown; y++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
